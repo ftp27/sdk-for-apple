1c3c65b418ea0407a0564a96c3fbb0c76cbe72a0
Aleksei Cherepanov <ftp27host@gmail.com>
2022-10-13 17:05:26 +0300

    iOS target SDK updated

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Appwrite",
     platforms: [
-        .iOS("15.0"),
+        .iOS("13.0"),
         .macOS("11.0"),
         .watchOS("6.0"),
         .tvOS("13.0"),
